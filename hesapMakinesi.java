82c1f150f11dafc62bbc19f53380c2fce8366b56
HP <20060339@stu.omu.edu.tr>
2023-03-01 20:37:19 +0300

    switch

--- hesapMakinesi.java
+++ hesapMakinesi.java
@@ -4,7 +4,23 @@ import java.util.Scanner;
  */
 public class hesapMakinesi {
   public static void main(String[] args) {
+    int sayi1, sayi2;
+
     Scanner input = new Scanner(System.in);
-    
+
+    System.out.print("İlk Sayıyı Giriniz : ");
+    sayi1 = input.nextInt();
+    System.out.println("İlk Sayı : " + sayi1);
+
+    System.out.print("İkinci Sayıyı Giriniz : ");
+    sayi2 = input.nextInt();
+
+    System.out.println("İlk Sayı : " + sayi1);
+    System.out.println("İkinci Sayı : " + sayi2);
+
+    System.out.println("Yapmak İstediğiniz İşlemi Seçin : ");
+    System.out.println("1-Toplama /n 2-Çıkarma /n 3-Çarpma /n 4-Bölme");
+
+
   }
 }
