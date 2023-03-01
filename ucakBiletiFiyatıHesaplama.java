31b3bb9215c05b4291740203bedc12d553dd8bc3
HP <20060339@stu.omu.edu.tr>
2023-03-01 23:30:45 +0300

    get information from the user

--- ucakBiletiFiyatıHesaplama.java
+++ ucakBiletiFiyatıHesaplama.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ucakBiletiFiyatıHesaplama {
   public static void main(String[] args) {
+
+    int mesafe, yas, yolculukTipi;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Mesafeyi km türünden giriniz : ");
+    mesafe = input.nextInt();
+
+    System.out.print("Yaşınızı giriniz : ");
+    yas = input.nextInt();
+
+    System.out.print("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
+    yolculukTipi = input.nextInt();
     
   }
   
