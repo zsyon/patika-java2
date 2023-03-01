1cf90aced46e492367a5807f7bbe62df792b94b1
HP <20060339@stu.omu.edu.tr>
2023-03-01 21:25:28 +0300

    add get data from user

---
+++ kullaniciGirsi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class kullaniciGirsi {
+  public static void main(String[] args) {
+
+    String kullaniciAdi;
+    int sifre; 
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Kullanıcı Adını Giriniz : ");
+
+    kullaniciAdi = input.nextLine();
+    sifre = input.nextInt();
+
+
+
+
+
+    
+  }
+  
+}
