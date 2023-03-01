2f6e1b31863cc072538b6279b99e02882d4ef4f1
HP <20060339@stu.omu.edu.tr>
2023-03-01 23:22:09 +0300

    add number

--- sayilariSiralama.java
+++ sayilariSiralama.java
@@ -1,5 +1,17 @@
 public class sayilariSiralama {
   public static void main(String[] args) {
+
+    int sayi1 = 10, sayi2 = 5, sayi3 = 3;
+
+    if(sayi1<sayi2 && sayi1<sayi3){
+      System.out.println("En kücük sayi " + sayi1);
+    }
+    else if(sayi2<sayi1 && sayi2<sayi3){
+      System.out.println("En küçük sayi " + sayi2);
+    }
+    else{
+      System.out.println("En küçük sayi " + sayi3);
+    }
     
   }
   
